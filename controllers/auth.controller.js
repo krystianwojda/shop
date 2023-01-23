81345575d5edb6333ca7f1ed7cebafe65d24b6a7
Krystian <krystian.wojda-98@wp.pl>
2023-01-23 17:18:33 +0100

    Adding User Signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,26 @@
+const User = require('../models/user.model');
+
 const getSignup = (req, res) => {
     res.render('customer/auth/signup');
 };
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
+    const user = new User(
+        req.body.email,
+        req.body.password,
+        req.body.fullname,
+        req.body.street,
+        req.body.postal,
+        req.body.city
+    );
 
+    await user.signUp();
+
+    res.redirect('/login');
 };
 
 const getLogin = (req, res) => {
-
+    res.render('customer/auth/login');
 };
 
 module.exports = {
---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+
+const db = require('../data/database');
+
+class User {
+    constructor(email, password, name, fullname, street, postal, city) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.fullname = fullname;
+        this.addres = {
+            street: street,
+            postal: postal,
+            city: city
+        };
+    }
+
+    async signUp() {
+        const hashedPassword = await bcrypt.hash(this.password, 12);
+
+        const result = await db.getDb().collection('users').insertOne({
+            email: this.email,
+            password: this.password,
+            name: this.name,
+            address: this.addres
+        });
+    };
+}
+
+module.exports = User;
